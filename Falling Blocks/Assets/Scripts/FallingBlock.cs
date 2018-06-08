586dca2b47f2015d9348f72d7d8cacd9bc69bdd7
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-08 15:20:51 +0300

    Add functionality for destroying objects after they go off screen (ep16)

--- Falling Blocks/Assets/Scripts/FallingBlock.cs
+++ Falling Blocks/Assets/Scripts/FallingBlock.cs
@@ -7,14 +7,20 @@ public class FallingBlock : MonoBehaviour {
     public Vector2 speedMinMax; // Variable min and max speed based on the difficulty % - editable in Inspector.
     float speed; // Variable for speed
 
+    float visibleHeightThreshold;
+
     private void Start() {
         speed = Mathf.Lerp(speedMinMax.x, speedMinMax.y, Difficulty.GetDifficultyPercent()); // Linearly interpolates between min and max speed by the difficulty % - To increase the speed make it more diffcilut over time.
+
+        visibleHeightThreshold = -Camera.main.orthographicSize - transform.localScale.y;
     }
 
     // Update is called once per frame
     void Update () {
         transform.Translate(Vector2.down * speed * Time.deltaTime, Space.Self); // Making the blocks falling in -Y direction by their speed in self space (for the rotation to be in local space).
 
-
+        if(transform.position.y < visibleHeightThreshold) {
+            Destroy(gameObject);
+        }
 	}
 }
