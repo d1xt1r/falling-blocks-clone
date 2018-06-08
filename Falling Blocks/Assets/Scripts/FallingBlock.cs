3c5a7e7fe9a2e4e9639675c0a7837574db6f98bc
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-08 11:40:21 +0300

    Revert "Revert "Add falling block script, spawner script, folders, prefabs.""
    This reverts commit 9913f5450350d375031e97268448ba292faf1afb.

---
+++ Falling Blocks/Assets/Scripts/FallingBlock.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBlock : MonoBehaviour {
+
+    float speed = 7f; // Initialising the speed of the falling blocs
+
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.down * speed * Time.deltaTime, Space.Self); // Making the blocks falling in -Y direction by their speed in self space (for the rotation to be in local space).
+
+
+	}
+}
